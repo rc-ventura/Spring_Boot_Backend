4c3579fca1ce88c6a8f45faf0334e51b04c6b5a9
rc-ventura <87483916+rc-ventura@users.noreply.github.com>
2022-09-06 14:42:42 -0300

    Create Endereco.java

---
+++ src/main/java/br/ufsc/curso/AulaSpringBoot/Entities/Endereco.java
@@ -0,0 +1,115 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.ufsc.curso.AulaSpringBoot.Entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author RC_Ve
+ */
+@Entity
+public class Endereco implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    
+//atributos 
+    
+     @Id   
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private Long id;
+    
+   
+    private String tipo;
+    private String rua;
+    private String numero;
+    private String cep;
+    
+    
+//construtores    
+    
+    public Endereco () {}
+
+    public Endereco(Long id, String tipo, String rua, String numero, String cep) {
+        this.id = id;
+        this.tipo = tipo;
+        this.rua = rua;
+        this.numero = numero;
+        this.cep = cep;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Endereco other = (Endereco) obj;
+        return Objects.equals(this.id, other.id);
+    }
+
+   
+    
+    
+    
+}
