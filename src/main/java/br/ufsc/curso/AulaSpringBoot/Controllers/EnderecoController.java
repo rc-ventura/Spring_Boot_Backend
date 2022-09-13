0ea7ec1d1f86ff212fc2b44bac57211c1229c584
rc-ventura <87483916+rc-ventura@users.noreply.github.com>
2022-09-13 09:44:43 -0300

    Update EnderecoController.java
    V1.1

--- src/main/java/br/ufsc/curso/AulaSpringBoot/Controllers/EnderecoController.java
+++ src/main/java/br/ufsc/curso/AulaSpringBoot/Controllers/EnderecoController.java
@@ -6,12 +6,18 @@ package br.ufsc.curso.AulaSpringBoot.Controllers;
 
 import br.ufsc.curso.AulaSpringBoot.Entities.Endereco;
 import br.ufsc.curso.AulaSpringBoot.Services.EnderecoService;
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  *
@@ -34,4 +40,32 @@ public class EnderecoController {
         Endereco end = enderecoService.findById(id);
         return ResponseEntity.ok().body(end);
     }
-}
+    
+    @PostMapping(value = "/enderecos")
+    public ResponseEntity<Endereco> save(@RequestBody Endereco end) {
+        Endereco savedEndereco = enderecoService.save(end);
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path
+        ("/enderecos/{id}").buildAndExpand(savedEndereco.getId()).toUri();
+        return ResponseEntity.created(uri).body(savedEndereco);
+    }
+        
+    @DeleteMapping(value = "/enderecos/{id}") 
+    public ResponseEntity<Void> delete(@PathVariable long id){
+        enderecoService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+    
+    
+    @PutMapping(value = "/enderecos/{id}")
+    public ResponseEntity<Endereco> update(@PathVariable Long id, @RequestBody Endereco end){
+       end = enderecoService.update(id, end);
+       return ResponseEntity.ok().body(end);
+   
+        
+    }
+        
+        
+        
+    }
+    
+
