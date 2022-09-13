9717def120ba04a85a5dd4a68ddc08b9efc4dcf8
rc-ventura <87483916+rc-ventura@users.noreply.github.com>
2022-09-13 09:44:49 -0300

    Update ClienteController.java
    V1.1

--- src/main/java/br/ufsc/curso/AulaSpringBoot/Controllers/ClienteController.java
+++ src/main/java/br/ufsc/curso/AulaSpringBoot/Controllers/ClienteController.java
@@ -6,12 +6,18 @@ package br.ufsc.curso.AulaSpringBoot.Controllers;
 
 import br.ufsc.curso.AulaSpringBoot.Entities.Cliente;
 import br.ufsc.curso.AulaSpringBoot.Services.ClienteService;
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  *
@@ -39,5 +45,27 @@ public class ClienteController {
 
         
     }
+    
+    @PostMapping(value = "/clientes")
+    public ResponseEntity<Cliente> save(@RequestBody Cliente cliente){
+        Cliente savedCliente = clienteService.save(cliente);
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path
+        ("/clientes/{id}").buildAndExpand(savedCliente.getId()).toUri();
+        return ResponseEntity.created(uri).body(savedCliente);
+    }
+    
+    
+    @DeleteMapping(value = "/clientes/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        clienteService.delete(id);
+        return ResponseEntity.noContent().build();
+        
+    }
+    
+    @PutMapping(value = "/clientes/{id}")
+    public ResponseEntity<Cliente> update (@PathVariable Long id, @RequestBody Cliente cliente){ 
+    cliente = clienteService.update(id, cliente);
+    return ResponseEntity.ok().body(cliente);
+}
     
 }
