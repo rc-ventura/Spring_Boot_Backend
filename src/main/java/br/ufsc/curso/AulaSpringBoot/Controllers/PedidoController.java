ea0c87f23cd13a9b747797defeddbd846b9356c3
rc-ventura <87483916+rc-ventura@users.noreply.github.com>
2022-09-17 19:27:26 -0300

    Update PedidoController.java

--- src/main/java/br/ufsc/curso/AulaSpringBoot/Controllers/PedidoController.java
+++ src/main/java/br/ufsc/curso/AulaSpringBoot/Controllers/PedidoController.java
@@ -2,9 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package br.ufsc.curso.AulaSpringBoot.Controllers;
-
 
 import br.ufsc.curso.AulaSpringBoot.Entities.Pedido;
 import br.ufsc.curso.AulaSpringBoot.services.PedidoService;
@@ -25,52 +23,43 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
  *
  * @author RC_Ventura
  */
-
 @RestController
 public class PedidoController {
-    
+
     @Autowired
     private PedidoService pedidoService;
-    
-    
+
     @GetMapping(value = "/pedidos")
-    public ResponseEntity<List<Pedido>> findAll(){
+    public ResponseEntity<List<Pedido>> findAll() {
         List<Pedido> pedidos = pedidoService.findAll();
         return ResponseEntity.ok().body(pedidos);
-}
+    }
 
     @GetMapping(value = "/pedidos/{id}")
-    public ResponseEntity <Pedido> findById(@PathVariable Long id){
+    public ResponseEntity<Pedido> findById(@PathVariable Long id) {
         Pedido pedido = pedidoService.findById(id);
         return ResponseEntity.ok().body(pedido);
 
-        
     }
-    
+
     @PostMapping(value = "/pedidos")
-    public ResponseEntity<Pedido> save(@RequestBody Pedido pedido){
-        Pedido savedPedido= pedidoService.save(pedido);
-        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path
-        ("/pedidos/{id}").buildAndExpand(savedPedido.getId()).toUri();
+    public ResponseEntity<Pedido> save(@RequestBody Pedido pedido) {
+        Pedido savedPedido = pedidoService.save(pedido);
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/pedidos/{id}").buildAndExpand(savedPedido.getId()).toUri();
         return ResponseEntity.created(uri).body(savedPedido);
     }
-    
-    
+
     @DeleteMapping(value = "/pedidos/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         pedidoService.deleteById(id);
         return ResponseEntity.noContent().build();
-        
+
     }
-    
+
     @PutMapping(value = "/pedidos/{id}")
-    public ResponseEntity<Pedido> update (@PathVariable Long id, @RequestBody Pedido pedido){ 
+    public ResponseEntity<Pedido> update(@PathVariable Long id, @RequestBody Pedido pedido) {
         pedido = pedidoService.update(id, pedido);
         return ResponseEntity.ok().body(pedido);
-}
-    
-            
-}
+    }
 
-   
-   
+}
