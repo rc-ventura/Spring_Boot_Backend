28b72e3e26ecffa6ae9a4282e7796ff897587e2c
rc-ventura <87483916+rc-ventura@users.noreply.github.com>
2022-09-17 19:27:38 -0300

    Update ClienteService.java

--- src/main/java/br/ufsc/curso/AulaSpringBoot/Services/ClienteService.java
+++ src/main/java/br/ufsc/curso/AulaSpringBoot/Services/ClienteService.java
@@ -1,4 +1,3 @@
-
 package br.ufsc.curso.AulaSpringBoot.services;
 
 import br.ufsc.curso.AulaSpringBoot.Entities.Cliente;
@@ -11,77 +10,69 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
-
 @Service
 public class ClienteService {
-	
-	@Autowired
-	private ClienteRepository clienteRepository; 
-	
-	@Autowired
-	private EnderecoRepository enderecoRepository;
-	
-	public Cliente addEndereco(Long idCliente, Long idEndereco) {
-		Cliente cliente = clienteRepository.findById(idCliente).get();		
-		Endereco endereco = enderecoRepository.findById(idEndereco).get();
-		
-		endereco.setCliente(cliente);
-		enderecoRepository.save(endereco);
 
-		return cliente;
-	}
-	
-	public Cliente removeEndereco(Long idCliente, Long idEndereco) {
-		Cliente cliente = clienteRepository.findById(idCliente).get();		
-		Endereco endereco = enderecoRepository.findById(idEndereco).get();
-		
-		endereco.setCliente(null);
-		enderecoRepository.save(endereco);
-		
-		return cliente;
-	}
-	
-	
-	
-	public Cliente update(Long id, Cliente cliente) {
-		Cliente clienteEntity = clienteRepository.getReferenceById(id);
-		
-		clienteEntity.setEmail(cliente.getEmail());
-		clienteEntity.setFone(cliente.getFone());
-		clienteEntity.setNome(cliente.getNome());
-		clienteEntity.setSenha(cliente.getSenha());
-		
-		return clienteRepository.save(clienteEntity);		
-	}
-	
-	
-	public void deleteById(Long id) {
-		//try {
-			clienteRepository.deleteById(id);
-		//}catch (RuntimeException e) {
-		//	e.printStackTrace();
-		//}
-		
-	}
-	
-	
-	public Cliente save(Cliente cliente) {
-		return clienteRepository.save(cliente);
-	}
-	
-	public List<Cliente> findAll(){
-		return clienteRepository.findAll();
-	}
+    @Autowired
+    private ClienteRepository clienteRepository;
 
-	public Cliente findById(Long id) {		
-		try {
-			return clienteRepository.findById(id).get();
-		} catch(NoSuchElementException e) {
-			throw new EntityNotFoundException("EntityNotFoundException Cliente id: " + id);
-		}
-	}
+    @Autowired
+    private EnderecoRepository enderecoRepository;
 
-    
-	
+    public Cliente addEndereco(Long idCliente, Long idEndereco) {
+        Cliente cliente = clienteRepository.findById(idCliente).get();
+        Endereco endereco = enderecoRepository.findById(idEndereco).get();
+
+        endereco.setCliente(cliente);
+        enderecoRepository.save(endereco);
+
+        return cliente;
+    }
+
+    public Cliente removeEndereco(Long idCliente, Long idEndereco) {
+        Cliente cliente = clienteRepository.findById(idCliente).get();
+        Endereco endereco = enderecoRepository.findById(idEndereco).get();
+
+        endereco.setCliente(null);
+        enderecoRepository.save(endereco);
+
+        return cliente;
+    }
+
+    public Cliente update(Long id, Cliente cliente) {
+        Cliente clienteEntity = clienteRepository.getReferenceById(id);
+
+        clienteEntity.setEmail(cliente.getEmail());
+        clienteEntity.setFone(cliente.getFone());
+        clienteEntity.setNome(cliente.getNome());
+        clienteEntity.setSenha(cliente.getSenha());
+
+        return clienteRepository.save(clienteEntity);
+    }
+
+    public void deleteById(Long id) {
+        //try {
+        clienteRepository.deleteById(id);
+        //}catch (RuntimeException e) {
+        //	e.printStackTrace();
+        //}
+
+    }
+
+    public Cliente save(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> findAll() {
+        return clienteRepository.findAll();
+    }
+
+    public Cliente findById(Long id) {
+        try {
+            return clienteRepository.findById(id).get();
+        } catch (NoSuchElementException e) {
+            throw new EntityNotFoundException("EntityNotFoundException Cliente id: " + id);
+        }
+    }
+
 }
