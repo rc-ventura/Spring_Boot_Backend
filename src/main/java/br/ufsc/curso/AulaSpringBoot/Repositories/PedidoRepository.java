857f7e3c46be398e752d13d3bff6ea9a853fcfcb
rc-ventura <87483916+rc-ventura@users.noreply.github.com>
2022-09-17 18:01:27 -0300

    Create PedidoRepository.java
    v1.0

---
+++ src/main/java/br/ufsc/curso/AulaSpringBoot/Repositories/PedidoRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.ufsc.curso.AulaSpringBoot.Repositories;
+
+import br.ufsc.curso.AulaSpringBoot.Entities.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author RC_Ve
+ */
+public interface PedidoRepository extends JpaRepository <Pedido, Long>{
+
+}
+    
+
